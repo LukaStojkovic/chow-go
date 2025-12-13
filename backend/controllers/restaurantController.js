b557a5fbabf790b63b1c0c5cb8a5e39769cf16c6
Luka Stojkovic <lukastojkovic006.ls@gmail.com>
2025-12-14 00:22:49 +0100

    Remove duplicated expres.json() line, add check if seller who add menu item is owner of restaurant

--- backend/controllers/restaurantController.js
+++ backend/controllers/restaurantController.js
@@ -9,8 +9,17 @@ export async function createMenuItem(req, res, next) {
     return next(new AppError("Missing required fields", 400));
   }
 
-  if (price < 0) {
+  if (price <= 0) {
     return next(new AppError("Price must be positive", 400));
+  }
+
+  const restaurant = await Restaurant.findById(restaurantId);
+
+  if (restaurant.ownerId.toString() !== req.user._id.toString()) {
+    throw new AppError(
+      "Not authorized to add menu items to this restaurant",
+      403
+    );
   }
 
   const newMenuItem = {
@@ -22,9 +31,9 @@ export async function createMenuItem(req, res, next) {
     imageUrls: Array.isArray(imageUrls) ? imageUrls : [],
   };
 
-  await Restaurant.findByIdAndUpdate(restaurantId, {
-    $push: { menuItems: newMenuItem },
-  });
+  restaurant.menuItems.push(newMenuItem);
+
+  await restaurant.save();
 
   res.status(201).json({
     success: true,
--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,6 @@ const limiter = rateLimit({
 
 // MIDDLEWARES
 app.use("/api", limiter);
-app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
