5fc6b7210d20cf61fc21238e8d97d877cf1a3620
Luka Stojkovic <lukastojkovic006.ls@gmail.com>
2026-04-10 00:38:15 +0200

    Fix string pattern

--- backend/index.js
+++ backend/index.js
@@ -53,10 +53,10 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/restaurant/orders", restaurantOrderRoutes);
 app.use("/api/courier/orders", courierOrderRoutes);
 
-if(process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get('/*', (req, res) => {
+  app.get("/:path(*)", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
