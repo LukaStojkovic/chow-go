1e600abacc9e65f5ac4fa0f9fb9f737608d65bb9
Luka Stojkovic <lukastojkovic006.ls@gmail.com>
2026-07-12 20:12:18 +0200

    Comment out rate limiting for test

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ const limiter = rateLimit({
 });
 
 // MIDDLEWARES
-app.use("/api", limiter);
+// app.use("/api", limiter); TO-DO: find better approach
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
